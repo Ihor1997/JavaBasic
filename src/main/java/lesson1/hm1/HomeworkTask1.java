ed251e05742701e63a1305f2add9fe7bb9c5d0c1
IhorTiurdo <ihorti@papayaglobal.com>
2021-10-12 13:12:47 +0300

    add fix to homework1

--- src/main/java/lesson1/hm1/HomeworkTask1.java
+++ src/main/java/lesson1/hm1/HomeworkTask1.java
@@ -6,8 +6,11 @@ public class HomeworkTask1 {
 
 
     public static void main(String[] args) {
-        int number = 75;
-        MathOperationUtils.checkEvenNumber(number);
+          int number = 76;
+          String operationResult =
+                (number != 0 && number % 2 == 0) ? "Number is even: "+number: "Number is not even: "+number;
+        System.out.println(operationResult);
+
     }
 
 
--- src/main/java/lesson1/hm1/HomeworkTask2.java
+++ src/main/java/lesson1/hm1/HomeworkTask2.java
@@ -2,10 +2,25 @@ package lesson1.hm1;
 
 import utils.MathOperationUtils;
 
+import java.util.Arrays;
+
 public class HomeworkTask2 {
 
     public static void main(String[] args) {
-        Integer[] intArray = {1,5,2,3,22,12};
-        MathOperationUtils.sortNumbersInArray(intArray, true);
+        //first solution
+        int a =5,b=4,c=7;
+        Integer[] intArray = {a,b,c};
+        Arrays.sort(intArray);
+        System.out.println(Arrays.toString(intArray));
+
+        //second solution
+
+        if (a<b & b<c) System.out.println(a + " " + b + " " + c);
+        if (a<c & c<b) System.out.println(a + " " + c + " " + b);
+        if (b<a & a<c) System.out.println(b + " " + a + " " + c);
+        if (b<c & c<a) System.out.println(b + " " + c + " " + a);
+        if (c<a & a<b) System.out.println(c + " " + a + " " + b);
+        if (c<b & b<a) System.out.println(c + " " + b + " " + a);
+
     }
 }
